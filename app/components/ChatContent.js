c409227294244ff25653cabfde52c9103b35493d
Alan Renteria <arenteriaperulles@outlook.com>
2016-07-13 01:07:59 -0700

    added proptypes

--- app/components/ChatContent.js
+++ app/components/ChatContent.js
@@ -1,6 +1,6 @@
 
 var React  = require('react');
-
+var PropTypes = React.PropTypes;
 
 var ChatContent = React.createClass({
 
@@ -11,11 +11,19 @@ var ChatContent = React.createClass({
       <li className="list-group-item clearfix">
 
         <div   role="group">
-       {this.props.task}:  {this.props.comment}
+       {this.props.user}:  {this.props.comment}
           </div>
       </li>
 
   		);
   	}
 });
+
+ChatContent.propTypes = {
+  user: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
+}
+
+
+
 module.exports = ChatContent;
