dad638cca871f2b5588acc86b5e60ccf960397d8
Alan Renteria <arenteriaperulles@outlook.com>
2016-07-15 03:46:57 -0700

    added server

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
+  }, devServer: {
+    contentBase: './dist',
+    hot: true
   },
   plugins: [HtmlWebpackPluginConfig]
 }
